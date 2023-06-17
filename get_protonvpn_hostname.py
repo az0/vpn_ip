6bdbde89d313e3b9ef87c95e77397cb5a153be58
Andrew Ziem <ahz001@gmail.com>
2023-06-17 15:59:26 -0600

    Add ProtonVPN hostnames via API

---
+++ get_protonvpn_hostname.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+url ="https://api.protonmail.ch/vpn/logicals"
+output_file = 'data/hostname/protonvpn_api.txt'
+
+response = requests.get(url)
+data = json.loads(response.text)
+
+domains0 = []
+for server in data['LogicalServers']:
+    if 'Domain' in server:
+        domain = server['Domain']
+        domains0.append(domain)
+
+print(f'extracted {len(domains0)} domains from {url}')
+
+with open(output_file, "w") as file:
+    domains1 = set(domains0)
+    for domain in sorted(domains1):
+        file.write(f'{domain}\n')
+
+print(f"{len(domains1)} domains written to {output_file}")
+
