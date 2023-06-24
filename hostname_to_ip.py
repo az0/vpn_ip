28155412f5e2f33e24814df76495310eb5f5d9e8
Andrew Ziem <ahz001@gmail.com>
2023-06-23 21:32:36 -0600

    Avoid intermediate file

--- hostname_to_ip.py
+++ hostname_to_ip.py
@@ -8,14 +8,15 @@ import os
 import socket
 
 hostname_dir = 'data/input/hostname_ip'
-int_fn ='data/hostname_ip.tmp' # intermediate, contains duplicate IPs
-final_fn = 'data/output/ip.txt' # final, one line per IP, no duplicate IPs
+final_fn = 'data/output/ip.txt'  # final, one line per IP, no duplicate IPs
 
 # https://www.cloudflare.com/en-gb/ips/
 # https://en.wikipedia.org/wiki/1.1.1.1
 # https://bgp.he.net/AS13335#_prefixes
 # https://en.wikipedia.org/wiki/Google_Public_DNS
 # https://bgp.he.net/AS15169#_prefixes
+
+
 def check_ip_in_ranges(ip):
     ranges = (
         "1.0.0.0/24",
@@ -44,7 +45,6 @@ def check_ip_in_ranges(ip):
     return False
 
 
-
 def read_hosts(directory):
     """Read domains, one line per file"""
     hosts = []
@@ -57,58 +57,50 @@ def read_hosts(directory):
                     line = line.strip()
                     if line.startswith("#"):
                         continue
-                    hostname = line.split('#')[0] # remove comment at end
+                    hostname = line.split('#')[0]  # remove comment at end
                     hostname = line.split(',')[0]
                     if not len(hostname) > 5:
                         continue
-                    if '-tor.' in hostname: #example: hostname=us-co-21-tor.protonvpn.net
+                    if '-tor.' in hostname:  # example: hostname=us-co-21-tor.protonvpn.net
                         print(f'WARNING: skipping tor: {hostname}')
                         continue
                     hosts.append(hostname)
     return set(hosts)
 
+
 hosts = read_hosts(hostname_dir)
 
+
 def resolve_hosts(hosts):
-    with open(int_fn, "w") as output_file:
-        for hostname in sorted(hosts):
-            # Attempt to resolve the hostname to an IP address
-            print (f'INFO: hostname={hostname}')
-            try:
-                ip_addresses = socket.getaddrinfo(hostname, None)
-            except socket.gaierror as e:
-                print(f"ERROR: Error resolving {hostname}: {e}")
+    ip_to_hostnames = collections.defaultdict(set)
+
+    for hostname in sorted(hosts):
+        # Attempt to resolve the hostname to an IP address
+        print(f'INFO: hostname={hostname}')
+        try:
+            ip_addresses = socket.getaddrinfo(hostname, None)
+        except socket.gaierror as e:
+            print(f"ERROR: Error resolving {hostname}: {e}")
+            continue
+        # Loop over each IP address and add it to the ip_to_hostnames dictionary
+        for ip_address in sorted(ip_addresses):
+            ip_addr = ip_address[4][0]
+            if check_ip_in_ranges(ip_addr):
+                print(f'WARNING: in whitelist {ip_addr} = {hostname}')
                 continue
-            # Loop over each IP address and write it to the output file
-            for ip_address in sorted(ip_addresses):
-                ip_addr = ip_address[4][0]
-                if check_ip_in_ranges(ip_addr):
-                    print(f'WARNING: in whitelist {ip_addr} = {hostname}')
-                    continue
-                output_file.write(f'{ip_addr} # {hostname}\n')
+            ip_to_hostnames[ip_addr].add(hostname)
+
+    # Sort the IP addresses
+    sorted_ips = sorted(ip_to_hostnames.keys(),
+                        key=lambda ip: int(ipaddress.ip_address(ip)))
+
+    print(f'INFO: count of IPs: {len(sorted_ips)}')
+
+    with open(final_fn, "w") as output_file:
+        # Loop over the sorted IP addresses and write each one to the output file
+        for ip in sorted_ips:
+            hostnames = ",".join(sorted(ip_to_hostnames[ip]))
+            output_file.write(f"{ip} # {hostnames}\n")
+
 
 resolve_hosts(hosts)
-
-# Open the input file for reading
-with open(int_fn, "r") as input_file:
-    # Read the lines into a list
-    lines = input_file.readlines()
-
-# Define a dictionary to hold the IP addresses and hostnames
-ip_to_hostnames = collections.defaultdict(set)
-
-# Loop over the lines and add each IP address and hostname to the dictionary
-for line in lines:
-    ip, _, hostname = line.strip().partition(" # ")
-    ip_to_hostnames[ip].add(hostname)
-
-# Sort the IP addresses
-sorted_ips = sorted(ip_to_hostnames.keys(), key=lambda ip: int(ipaddress.ip_address(ip)))
-
-# Open the output file for writing
-with open(final_fn, "w") as output_file:
-    # Loop over the sorted IP addresses and write each one to the output file
-    for ip in sorted_ips:
-        hostnames = ",".join(sorted(ip_to_hostnames[ip]))
-        output_file.write(f"{ip} # {hostnames}\n")
-
