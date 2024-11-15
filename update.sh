d5a82687e953c1f572a0cb760afa6b031cfece64
Andrew Ziem <ahz001@gmail.com>
2024-11-14 21:47:01 -0700

    Run Umbrella

--- update.sh
+++ update.sh
@@ -18,6 +18,9 @@ echo "$(date): Running get_addresses_via_api.py"
 echo "$(date): Running get_browsec_github.py"
 time timeout 3m ./get_browsec_github.py || exit 1
 
+echo "$(date): Running get_cisco_umbrella.py"
+./get_cisco_umbrella.py || exit 1
+
 echo "$(date): Running prepare_final_lists.py"
 ./prepare_final_lists.py || exit 1
 
